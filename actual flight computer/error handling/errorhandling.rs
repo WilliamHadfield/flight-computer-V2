3de808a028659b3f0af05577b3d55611744762f1
will H <wustracean@gmail.com>
2026-07-02 15:43:30 +0100

    Create errorhandling.rs

---
+++ actual flight computer/error handling/errorhandling.rs
@@ -0,0 +1,17 @@
+#[derive(defmt::Format, Error, Debug)]
+enum Imu_error {
+
+}
+#[derive(defmt::Format, Error, Debug)]
+enum baro_error {
+
+}
+
+#[derive(defmt::Format, Error, Debug)]
+enum gps_error {
+
+}
+#[derive(defmt::Format, Error, Debug)]
+enum mag_error {
+
+}
